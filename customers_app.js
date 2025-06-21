55ce73b05cf24b94cfac345deb5b9e688b6b6e78
Sakurakivn <166159218+Sakurakivn@users.noreply.github.com>
2025-06-21 10:09:20 -0700

    Update customers_app.js

--- customers_app.js
+++ customers_app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', 
+document.addEventListener('DOMContentLoaded', () => {
     let allCustomersData = {}; 
     let currentOpenCustomerOriginalName = null;
     let currentOpenCustomerId = null;
